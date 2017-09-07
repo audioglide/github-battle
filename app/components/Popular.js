13446231ec083a7fab3552719c56b526560b4519
Alin Tudosie <alin.tudosie@itsix.com>
2017-09-07 14:36:05 +0300

    Added new css styles

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -92,7 +92,7 @@ class Popular extends React.Component {
                     selectedLanguage={this.state.selectedLanguage}
                     onSelect={this.updateLanguage} />
                 {!this.state.repos
-                    ? <p>LOADING!</p>
+                    ? <div className='app-loader'><p>LOADING!</p></div>
                     : <RepoGrid repos={this.state.repos} />}
             </div>
         )
